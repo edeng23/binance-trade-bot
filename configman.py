813eb1840044455650501121b5f4378079daf582
Ariel Schon <ariel.schon12@gmail.com>
2020-12-05 12:38:50 +0200

    Changed configuration to be in a class, reading config file one time on startup only.

--- configman.py
+++ configman.py
@@ -1,53 +1,52 @@
 # Globals
 g_config_file = "config.txt"
 
-g_cfg_field_api_key = "api_key"
-g_cfg_field_api_secret = "api_secret_key"
+class ConfigMan():
 
-g_config_fields = [g_cfg_field_api_key, g_cfg_field_api_secret]
+    cfg_field_api_key = "api_key"
+    cfg_field_api_secret = "api_secret_key"
+    cfg_field_current_coin = "current_coin"
 
-def _generate_new_config_file():
-    # Generate a new config file
-    print("Generating new 'config.txt' file.")
+    cfg_fields = [cfg_field_api_key, cfg_field_api_secret, cfg_field_current_coin]
 
-    values = {}
-    for field in g_config_fields:
-        val = input("{}: ".format(field)).strip()
-        values[field] = val
+    def __init__(self):
+        # read config file
+        try:
+            with open(g_config_file, "r") as cf:
+                config_lines = cf.readlines()
+
+            values = {}
+            for cl in config_lines:
+                key, val = [s.strip() for s in cl.split("=")]
+                values[key] = val
+            
+            self.config_values = values
+        except:
+            # if file doesn't exist
+            values = self._generate_new_config_file()
+            self.config_values = values
+
+    def get_api_keys(self):
+        return self.config_values[self.cfg_field_api_key], self.config_values[self.cfg_field_api_secret]
     
-    config_lines = [k + "=" + v + "\n" for k,v in values.items()]
+    def get_current_coin(self):
+        return self.config_values[self.cfg_field_current_coin]
 
-    with open(g_config_file, "w") as cf:
-        cf.writelines(config_lines)
+    def _generate_new_config_file(self):
+        # Generate a new config file
+        print("Generating new 'config.txt' file.")
 
-    return values
+        values = {}
+        for field in self.cfg_fields:
+            val = input("{}: ".format(field)).strip()
+            values[field] = val
+        
+        config_lines = [k + "=" + v + "\n" for k,v in values.items()]
 
+        with open(g_config_file, "w") as cf:
+            cf.writelines(config_lines)
 
-def get_api_keys():
-    # parse the file and return public, private keys
-    try:
-        with open(g_config_file, "r") as cf:
-            config_lines = cf.readlines()
-    except:
-        # if we couldn't find the config file, generate a new one through user input
-        print("'config.txt' doesn't exist...")
-        config_vals = _generate_new_config_file()
-        return config_vals[g_cfg_field_api_key], config_vals[g_cfg_field_api_secret]
-
-    # look for api_key
-
-    api_key_line = list(filter(lambda s: s.find(g_cfg_field_api_key) != -1, config_lines))[0]
-    api_key = api_key_line.split("=")[1].strip()
-
-    # look for secret key
-    api_secret_key_line = list(filter(lambda s: s.find(g_cfg_field_api_secret) != -1, config_lines))[0]
-    api_secret_key = api_secret_key_line.split("=")[1].strip()
-
-    # sanity
-    if not api_secret_key or not api_key:
-        raise("No api keys found in config.txt!")
-
-    return api_key, api_secret_key
+        return values
 
 # should be imported...
 def main():
