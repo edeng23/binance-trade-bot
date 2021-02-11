a92dadf38bfbc40623cb79e30a27ee0970106f68
edeng23 <edencgaon@gmail.com>
2021-02-11 20:47:21 +0200

    Fixed APIError(code=-2013): Order does not exist.

--- crypto_trading.py
+++ crypto_trading.py
@@ -147,9 +147,13 @@ def buy_alt(client, alt_symbol, crypto_symbol):
             time.sleep(10)
 
     while stat[u'status'] != 'FILLED':
-        stat = client.get_order(
-            symbol=alt_symbol+crypto_symbol, orderId=order[u'orderId'])
-        time.sleep(1)
+        try:
+            stat = client.get_order(
+                symbol=alt_symbol+crypto_symbol, orderId=order[u'orderId'])
+            time.sleep(1)
+        except BinanceAPIException as e:
+            logger.info(e)
+            time.sleep(2)
 
     logger.info('Bought %s', alt_symbol)
 
