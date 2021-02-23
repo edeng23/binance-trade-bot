e9898f4cdd1796cbdfa85b7256dc01f49b84ef9b
James Waters <james@jcwaters.co.uk>
2021-02-23 22:43:46 +0000

    Add filtering by time period

--- api_server.py
+++ api_server.py
@@ -1,8 +1,9 @@
-from datetime import datetime
+import re
+from datetime import datetime, timedelta
 from itertools import groupby
 from typing import List, Tuple
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 from sqlalchemy import func
@@ -18,19 +19,41 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 
+def filter_period(query, model):
+    period = request.args.get('period', 'all')
+
+    if period == 'all':
+        return query
+
+    num = float(re.search(r"(\d*)[shdwm]", "1d").group(1))
+
+    if 's' in period:
+        return query.filter(model.datetime >= datetime.now() - timedelta(seconds=num))
+    if 'h' in period:
+        return query.filter(model.datetime >= datetime.now() - timedelta(hours=num))
+    if 'd' in period:
+        return query.filter(model.datetime >= datetime.now() - timedelta(days=num))
+    if 'w' in period:
+        return query.filter(model.datetime >= datetime.now() - timedelta(weeks=num))
+    if 'm' in period:
+        return query.filter(model.datetime >= datetime.now() - timedelta(days=28 * num))
+
+
 @app.route('/api/value_history/<coin>')
 @app.route('/api/value_history')
 def value_history(coin: str = None):
     session: Session
     with db_session() as session:
-        values = session.query(CoinValue).order_by(CoinValue.coin_id.asc(),
-                                                                    CoinValue.datetime.asc())
+        query = session.query(CoinValue).order_by(CoinValue.coin_id.asc(),
+                                                  CoinValue.datetime.asc())
+
+        query = filter_period(query, CoinValue)
 
         if coin:
-            values: List[CoinValue] = values.filter(CoinValue.coin_id == coin).all()
+            values: List[CoinValue] = query.filter(CoinValue.coin_id == coin).all()
             return jsonify([entry.info() for entry in values])
 
-        coin_values = groupby(values.all(), key=lambda cv: cv.coin)
+        coin_values = groupby(query.all(), key=lambda cv: cv.coin)
         return jsonify({coin.symbol: [entry.info() for entry in history] for coin, history in coin_values})
 
 
@@ -38,10 +61,14 @@ def value_history(coin: str = None):
 def total_value_history():
     session: Session
     with db_session() as session:
-        total_values: List[Tuple[datetime, float, float]] = session.query(CoinValue.datetime,
-                                                                          func.sum(CoinValue.btc_value),
-                                                                          func.sum(CoinValue.usd_value)).group_by(
-            CoinValue.datetime).all()
+        query = session.query(CoinValue.datetime,
+                              func.sum(CoinValue.btc_value),
+                              func.sum(CoinValue.usd_value)).group_by(
+            CoinValue.datetime)
+
+        query = filter_period(query, CoinValue)
+
+        total_values: List[Tuple[datetime, float, float]] = query.all()
         return jsonify([{"datetime": tv[0], "btc": tv[1], "usd": tv[2]} for tv in total_values])
 
 
@@ -49,7 +76,11 @@ def total_value_history():
 def trade_history():
     session: Session
     with db_session() as session:
-        trades: List[Trade] = session.query(Trade).order_by(Trade.datetime.asc()).all()
+        query = session.query(Trade).order_by(Trade.datetime.asc())
+
+        query = filter_period(query, Trade)
+
+        trades: List[Trade] = query.all()
         return jsonify([trade.info() for trade in trades])
 
 
@@ -59,8 +90,12 @@ def scouting_history():
     coin = current_coin.symbol if current_coin is not None else None
     session: Session
     with db_session() as session:
-        scouts: List[ScoutHistory] = session.query(ScoutHistory).join(ScoutHistory.pair).filter(
-            Pair.from_coin_id == coin).order_by(ScoutHistory.datetime.asc()).all()
+        query = session.query(ScoutHistory).join(ScoutHistory.pair).filter(
+            Pair.from_coin_id == coin).order_by(ScoutHistory.datetime.asc())
+
+        query = filter_period(query, ScoutHistory)
+
+        scouts: List[ScoutHistory] = query.all()
         return jsonify([scout.info() for scout in scouts])
 
 
@@ -74,7 +109,11 @@ def current_coin():
 def current_coin_history():
     session: Session
     with db_session() as session:
-        current_coins: List[CurrentCoin] = session.query(CurrentCoin).all()
+        query = session.query(CurrentCoin)
+
+        query = filter_period(query, CurrentCoin)
+
+        current_coins: List[CurrentCoin] = query.all()
         return jsonify([cc.info() for cc in current_coins])
 
 
