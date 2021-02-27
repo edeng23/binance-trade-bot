4aa204aa191b36789558a0b3c49b236b98940f20
segersniels <segers.n@hotmail.com>
2021-02-27 12:18:41 +0100

    :sparkles: Add support to override using environment variables

--- binance_trade_bot/config.py
+++ binance_trade_bot/config.py
@@ -24,27 +24,36 @@ class Config:
             exit()
         config.read(CFG_FL_NAME)
 
-        self.BRIDGE_SYMBOL = config.get(USER_CFG_SECTION, 'bridge')
+        self.BRIDGE_SYMBOL = os.environ.get('BRIDGE_SYMBOL') or config.get(
+            USER_CFG_SECTION, 'bridge')
         self.BRIDGE = Coin(self.BRIDGE_SYMBOL, False)
 
         # Prune settings
-        self.SCOUT_HISTORY_PRUNE_TIME = float(config.get(USER_CFG_SECTION, 'hourToKeepScoutHistory', fallback="1"))
+        self.SCOUT_HISTORY_PRUNE_TIME = float(os.environ.get('HOURS_TO_KEEP_SCOUTING_HISTORY') or config.get(
+            USER_CFG_SECTION, 'hourToKeepScoutHistory', fallback="1"))
 
         # Get config for scout
-        self.SCOUT_TRANSACTION_FEE = float(config.get(USER_CFG_SECTION, 'scout_transaction_fee'))
-        self.SCOUT_MULTIPLIER = float(config.get(USER_CFG_SECTION, 'scout_multiplier'))
-        self.SCOUT_SLEEP_TIME = int(config.get(USER_CFG_SECTION, 'scout_sleep_time'))
+        self.SCOUT_TRANSACTION_FEE = float(os.environ.get('SCOUT_TRANSACTION_FEE') or config.get(
+            USER_CFG_SECTION, 'scout_transaction_fee'))
+        self.SCOUT_MULTIPLIER = float(os.environ.get('SCOUT_MULTIPLIER') or config.get(
+            USER_CFG_SECTION, 'scout_multiplier'))
+        self.SCOUT_SLEEP_TIME = int(os.environ.get('SCOUT_SLEEP_TIME') or config.get(
+            USER_CFG_SECTION, 'scout_sleep_time'))
 
         # Get config for binance
-        self.BINANCE_API_KEY = config.get(USER_CFG_SECTION, 'api_key')
-        self.BINANCE_API_SECRET_KEY = config.get(USER_CFG_SECTION, 'api_secret_key')
-        self.BINANCE_TLD = config.get(USER_CFG_SECTION, 'tld')
+        self.BINANCE_API_KEY = os.environ.get('API_KEY') or config.get(
+            USER_CFG_SECTION, 'api_key')
+        self.BINANCE_API_SECRET_KEY = os.environ.get('API_SECRET_KEY') or config.get(
+            USER_CFG_SECTION, 'api_secret_key')
+        self.BINANCE_TLD = os.environ.get('TLD') or config.get(USER_CFG_SECTION, 'tld') or 'com'
 
-        self.SUPPORTED_COIN_LIST = []
+        self.SUPPORTED_COIN_LIST = os.environ.get('SUPPORTED_COIN_LIST').split() or []
 
         # Get supported coin list from supported_coin_list file
-        with open('supported_coin_list') as f:
-            self.SUPPORTED_COIN_LIST = f.read().upper().strip().splitlines()
-            self.SUPPORTED_COIN_LIST = list(filter(None, self.SUPPORTED_COIN_LIST))
+        if not self.SUPPORTED_COIN_LIST:
+            with open('supported_coin_list') as f:
+                self.SUPPORTED_COIN_LIST = f.read().upper().strip().splitlines()
+                self.SUPPORTED_COIN_LIST = list(filter(None, self.SUPPORTED_COIN_LIST))
 
-        self.CURRENT_COIN_SYMBOL = config.get(USER_CFG_SECTION, 'current_coin')
+        self.CURRENT_COIN_SYMBOL = os.environ.get('CURRENT_COIN_SYMBOL') or config.get(
+            USER_CFG_SECTION, 'current_coin')
