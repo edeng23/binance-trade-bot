9d4db23aac30fdbd4a41ddd33ce353260e320ea6
Aleksei Grigorovskii <agrigoro@estee.com>
2021-03-23 01:41:17 +0300

    Add logger.info about chosen strategy on bot start

--- binance_trade_bot/crypto_trading.py
+++ binance_trade_bot/crypto_trading.py
@@ -21,6 +21,7 @@ def main():
         logger.error("Invalid strategy name")
         return
     trader = strategy(manager, db, logger, config)
+    logger.info(f"Chosen strategy: {config.STRATEGY}")
 
     logger.info("Creating database schema if it doesn't already exist")
     db.create_database()
