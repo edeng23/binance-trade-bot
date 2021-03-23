e3e8837e85e52657a4a051adef035079803c00fa
Tamas Magyar <tamas.magyar.py@gmail.com>
2021-03-23 12:53:12 +0100

    lint fix

--- tests/config_test.py
+++ tests/config_test.py
@@ -1,48 +1,47 @@
 import os
-import time
 from configparser import NoOptionError
 from contextlib import contextmanager
-from unittest.mock import patch, mock_open
+from unittest.mock import mock_open, patch
 
 import pytest
 
-from binance_trade_bot.config import Config, USER_CFG_SECTION
-from binance_trade_bot.models import Coin
-from tests.test_data.user_config import DEFAULT_USER_CONFIG, API_KEY, SECRET_KEY, BRIDGE_SYMBOL, BINANCE_TLD
+from binance_trade_bot.config import CFG_FL_NAME, Config
+from tests.test_data.user_config import API_KEY, BINANCE_TLD, BRIDGE_SYMBOL, DEFAULT_USER_CONFIG, SECRET_KEY
 
 
 class TestConfig:
-
-    @contextmanager
-    def create_user_config_file(self, content=''):
-        file_path = "user.cfg"
-        with open(file_path, 'w') as f:
-            f.write(content)
-        try:
-            yield file_path
-            os.remove(file_path)
-        except Exception:
-            pass
-        finally:
-            if os.path.exists(file_path):
-                os.remove(file_path)
-
-    def test_config_open(self):
+    @staticmethod
+    def test_config_open():
         with pytest.raises(NoOptionError):
-            with patch('builtins.open', new=mock_open(read_data="")) as user_config_mock:
+            with patch("builtins.open", new=mock_open(read_data="")) as user_config_mock:
                 Config()
-                user_config_mock.assert_called_once_with('user.cfg', 'r')
+                user_config_mock.assert_called_once_with(CFG_FL_NAME, "r")
 
-    def test_config_read(self):
-        with self.create_user_config_file(DEFAULT_USER_CONFIG):
+    @staticmethod
+    def test_config_read():
+        with create_user_config_file(DEFAULT_USER_CONFIG):
             trade_bot_config = Config()
             assert trade_bot_config.BINANCE_API_KEY == API_KEY
             assert trade_bot_config.BINANCE_API_SECRET_KEY == SECRET_KEY
             assert trade_bot_config.BINANCE_TLD == BINANCE_TLD
             assert trade_bot_config.BRIDGE.symbol == BRIDGE_SYMBOL
             assert trade_bot_config.CURRENT_COIN_SYMBOL == ""
-            assert type(trade_bot_config.SUPPORTED_COIN_LIST) == list
+            assert isinstance(trade_bot_config.SUPPORTED_COIN_LIST, list)
             assert len(trade_bot_config.SUPPORTED_COIN_LIST) > 0
             assert trade_bot_config.SCOUT_MULTIPLIER == 5
             assert trade_bot_config.SCOUT_SLEEP_TIME == 5.0
             assert trade_bot_config.SCOUT_HISTORY_PRUNE_TIME == 1.0
+
+
+@contextmanager
+def create_user_config_file(content=""):
+    with open(CFG_FL_NAME, "w") as f:
+        f.write(content)
+    try:
+        yield CFG_FL_NAME
+        os.remove(CFG_FL_NAME)
+    except Exception:  # pylint: disable=broad-except
+        pass
+    finally:
+        if os.path.exists(CFG_FL_NAME):
+            os.remove(CFG_FL_NAME)
