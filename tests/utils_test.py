15a2b06f9e50ad79702a9f0d2deaa9f85d7a301c
Tamas Magyar <tamas.magyar.py@gmail.com>
2021-03-28 14:45:35 +0200

    reordered test data

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -2,11 +2,11 @@ from binance_trade_bot.utils import get_market_ticker_price_from_list
 
 
 class TestUtils:
+    ETC_BTC = {"symbol": "ETHBTC", "price": "0.07946600"}
+
     LTC_BTC_PRICE = 4.00000200
     LTC_BTC_SYMBOL = "LTCBTC"
     LTC_BTC = {"symbol": LTC_BTC_SYMBOL, "price": str(LTC_BTC_PRICE)}
-
-    ETC_BTC = {"symbol": "ETHBTC", "price": "0.07946600"}
 
     def test_get_market_price_ltc_btc(self):
         price = get_market_ticker_price_from_list([self.LTC_BTC, self.ETC_BTC], self.LTC_BTC_SYMBOL)
