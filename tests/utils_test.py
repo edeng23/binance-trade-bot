751a587b9c7684e9ae600f3fd666694ff046d45e
Tamas Magyar <tamas.magyar.py@gmail.com>
2021-03-28 14:55:26 +0200

    reordered methods

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -8,19 +8,19 @@ class TestUtils:
     LTC_BTC_SYMBOL = "LTCBTC"
     LTC_BTC = {"symbol": LTC_BTC_SYMBOL, "price": str(LTC_BTC_PRICE)}
 
-    def test_get_market_price_ltc_btc(self):
-        price = get_market_ticker_price_from_list([self.LTC_BTC, self.ETC_BTC], self.LTC_BTC_SYMBOL)
-        assert price == self.LTC_BTC_PRICE
-
     def test_get_market_price_empty_tickers_list(self):
         price = get_market_ticker_price_from_list([], self.LTC_BTC_SYMBOL)
         assert price is None
+
+    def test_get_market_price_ticker_not_found(self):
+        price = get_market_ticker_price_from_list([self.ETC_BTC], self.LTC_BTC_SYMBOL)
+        assert price is None
+
+    def test_get_market_price_ltc_btc(self):
+        price = get_market_ticker_price_from_list([self.LTC_BTC, self.ETC_BTC], self.LTC_BTC_SYMBOL)
+        assert price == self.LTC_BTC_PRICE
 
     def test_get_market_price_duplicate_symbols(self):
         duplicated_btc_ltc = {"symbol": self.LTC_BTC_SYMBOL, "price": "0.07946600"}
         price = get_market_ticker_price_from_list([self.LTC_BTC, duplicated_btc_ltc], self.LTC_BTC_SYMBOL)
         assert price == self.LTC_BTC_PRICE
-
-    def test_get_market_price_ticker_not_found(self):
-        price = get_market_ticker_price_from_list([self.ETC_BTC], self.LTC_BTC_SYMBOL)
-        assert price is None
