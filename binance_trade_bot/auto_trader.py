f95558d6e39c4a85074e618deb5fc11ebde3038e
Cameron Tetford <cameron.tetford+github@gmail.com>
2021-04-07 12:58:53 -0400

    Update auto_trader.py

--- binance_trade_bot/auto_trader.py
+++ binance_trade_bot/auto_trader.py
@@ -26,16 +26,16 @@ class AutoTrader:
         """
         available_tickers = {}
         for ticker in all_tickers:
-            available_tickers[ticker['symbol']] = None
-        if pair.from_coin_id+pair.to_coin_id in available_tickers:
+            available_tickers[ticker["symbol"]] = None
+        if pair.from_coin_id + pair.to_coin_id in available_tickers:
             self.logger.info(
                 "Direct pair {0}{1} exists. Selling {0} for {1}".format(pair.from_coin_id, pair.to_coin_id)
             )
-            result = self.manager.sell_alt(pair.from_coin, pair.to_coin);
+            result = self.manager.sell_alt(pair.from_coin, pair.to_coin)
             if result is not None:
                 self.update_trade_threshold(pair.to_coin, float(result["price"]), all_tickers)
                 return None
-        elif pair.to_coin_id+pair.from_coin_id in available_tickers:
+        elif pair.to_coin_id + pair.from_coin_id in available_tickers:
             self.logger.info(
                 "Direct pair {0}{1} exists. Buying {0} with {1}".format(pair.to_coin_id, pair.from_coin_id)
             )
