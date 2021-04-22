0e61283267bdf749166eef81cb7436d47767639b
Debian <admin@ip-172-31-1-92.eu-central-1.compute.internal>
2021-04-22 18:26:22 +0000

    Revert changes on strategies

--- binance_trade_bot/strategies/multiple_coins_strategy.py
+++ binance_trade_bot/strategies/multiple_coins_strategy.py
@@ -7,14 +7,6 @@ class Strategy(AutoTrader):
         Scout for potential jumps from the current coin to another coin
         """
         all_tickers = self.manager.get_all_market_tickers()
-        have_coin = False
-
-        # last coin bought
-        current_coin = self.db.get_current_coin()
-        current_coin_symbol = ""
-
-        if current_coin is not None:
-            current_coin_symbol = current_coin.symbol
 
         for coin in self.db.get_coins():
             current_coin_balance = self.manager.get_currency_balance(coin.symbol)
@@ -24,12 +16,10 @@ class Strategy(AutoTrader):
                 self.logger.info("Skipping scouting... current coin {} not found".format(coin + self.config.BRIDGE))
                 continue
 
-            min_notional = self.manager.get_min_notional(coin.symbol, self.config.BRIDGE.symbol)
-
-            if coin.symbol != current_coin_symbol and coin_price * current_coin_balance < min_notional:
+            if coin_price * current_coin_balance < self.manager.get_min_notional(
+                coin.symbol, self.config.BRIDGE.symbol
+            ):
                 continue
-
-            have_coin = True
 
             # Display on the console, the current coin+Bridge, so users can see *some* activity and not think the bot
             # has stopped. Not logging though to reduce log size.
@@ -37,5 +27,4 @@ class Strategy(AutoTrader):
 
             self._jump_to_best_coin(coin, coin_price, all_tickers)
 
-        if not have_coin:
-            self.bridge_scout()
+        self.bridge_scout()
