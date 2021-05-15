c96b5533de8cf53471b57135f31f6ddb0e31844a
Dmitry Kravtsov <idkravitz@gmail.com>
2021-05-16 00:57:03 +1000

    Protect against KeyError on balanceUpdate
    Got a report for KeyError on del balances with 'LDDOGE' as key, apparently this key doesn't correspond to any known spot asset and it should be safe to ignore such asset updates

--- binance_trade_bot/binance_stream_manager.py
+++ binance_trade_bot/binance_stream_manager.py
@@ -149,7 +149,9 @@ class BinanceStreamManager:
         elif event_type == "balanceUpdate":  # !userData
             self.logger.debug(f"Balance update: {stream_data}")
             with self.cache.open_balances() as balances:
-                del balances[stream_data["asset"]]
+                asset = stream_data["asset"]
+                if asset in balances:
+                    del balances[stream_data["asset"]]
         elif event_type in ("outboundAccountPosition", "outboundAccountInfo"):  # !userData
             self.logger.debug(f"{event_type}: {stream_data}")
             with self.cache.open_balances() as balances:
