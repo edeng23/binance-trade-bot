61ef068649bc61f64e76c853472166a43cb9bfa0
MasaiasuOse <45827629+MasaiasuOse@users.noreply.github.com>
2021-05-17 22:26:19 +0200

    Update auto_trader.py
    (cherry picked from commit cc3b1d65171327f974e28aec635d507a959f1806)

--- binance_trade_bot/auto_trader.py
+++ binance_trade_bot/auto_trader.py
@@ -82,7 +82,7 @@ class AutoTrader:
             for pair in session.query(Pair).filter(Pair.ratio.is_(None)).all():
                 if not pair.from_coin.enabled or not pair.to_coin.enabled:
                     continue
-                self.logger.info(f"Initializing {pair.from_coin} vs {pair.to_coin}")
+                self.logger.debug(f"Initializing {pair.from_coin} vs {pair.to_coin}")
 
                 from_coin_price = self.manager.get_ticker_price(pair.from_coin + self.config.BRIDGE)
                 if from_coin_price is None:
