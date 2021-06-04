0897edc19f31f18bb901feede9da7dbb701c5cba
Nico Lachmuth <nico@nlachmuth.de>
2021-06-04 03:41:27 +0200

    added ratio adjust strat. credits to the backtest boys :)

---
+++ binance_trade_bot/strategies/ratio_adjust_strategy.py
@@ -0,0 +1,126 @@
+import random
+import sys
+from datetime import datetime, timedelta
+from sqlalchemy.orm import Session, aliased
+from sqlalchemy.sql.expression import and_
+
+
+from binance_trade_bot.auto_trader import AutoTrader
+from binance_trade_bot.database import Pair, Trade, Coin
+
+
+class Strategy(AutoTrader):
+    def initialize(self):
+        super().initialize()
+        self.initialize_current_coin()
+        self.reinit_threshold = self.manager.now()
+        self.logger.info(f"CAUTION: The ratio_adjusst strategy is still work in progress and can lead to losses! Use this strategy only if you know what you are doing, did alot of backtests and can live with possible losses.")
+        self.logger.info(f"Using {self.config.MAX_IDLE_HOURS} hours to reinit the ratios.")
+
+    def scout(self):
+        #check if previous buy order failed. If so, bridge scout for a new coin.
+        if self.failed_buy_order:
+            self.bridge_scout()
+
+        max_idle_timeout = float(self.config.MAX_IDLE_HOURS)
+        base_time: datetime = self.manager.now()
+        allowed_idle_time = self.reinit_threshold + timedelta(hours=max_idle_timeout)
+        if base_time >= allowed_idle_time:
+            #self.logger.info(f"Last reinit was before {max_idle_timeout} hours! Going to reinit ratios.")
+            self.re_initialize_trade_thresholds()
+            #self.logger.debug("Finished reiniting the ratios.")
+            self.reinit_threshold = self.manager.now()                 
+
+        """
+        Scout for potential jumps from the current coin to another coin
+        """
+        current_coin = self.db.get_current_coin()
+        # Display on the console, the current coin+Bridge, so users can see *some* activity and not think the bot has
+        # stopped. Not logging though to reduce log size.
+        # print(
+        #     f"{self.manager.now()} - CONSOLE - INFO - I am scouting the best trades. "
+        #     f"Current coin: {current_coin + self.config.BRIDGE} ",
+        #     end="\r",
+        # )
+
+        current_coin_price = self.manager.get_sell_price(current_coin + self.config.BRIDGE)
+
+        if current_coin_price is None:
+            self.logger.info("Skipping scouting... current coin {} not found".format(current_coin + self.config.BRIDGE))
+            return
+
+        self._jump_to_best_coin(current_coin, current_coin_price)
+
+    def bridge_scout(self):
+        current_coin = self.db.get_current_coin()
+        if self.manager.get_currency_balance(current_coin.symbol) > self.manager.get_min_notional(
+            current_coin.symbol, self.config.BRIDGE.symbol
+        ):
+            # Only scout if we don't have enough of the current coin
+            return
+        new_coin = super().bridge_scout()
+        if new_coin is not None:
+            self.db.set_current_coin(new_coin)
+
+    def initialize_current_coin(self):
+        """
+        Decide what is the current coin, and set it up in the DB.
+        """
+        if self.db.get_current_coin() is None:
+            current_coin_symbol = self.config.CURRENT_COIN_SYMBOL
+            if not current_coin_symbol:
+                current_coin_symbol = random.choice(self.config.SUPPORTED_COIN_LIST)
+
+            self.logger.info(f"Setting initial coin to {current_coin_symbol}")
+
+            if current_coin_symbol not in self.config.SUPPORTED_COIN_LIST:
+                sys.exit("***\nERROR!\nSince there is no backup file, a proper coin name must be provided at init\n***")
+            self.db.set_current_coin(current_coin_symbol)
+
+            # if we don't have a configuration, we selected a coin at random... Buy it so we can start trading.
+            if self.config.CURRENT_COIN_SYMBOL == "":
+                current_coin = self.db.get_current_coin()
+                self.logger.info(f"Purchasing {current_coin} to begin trading")
+                self.manager.buy_alt(
+                    current_coin, self.config.BRIDGE, self.manager.get_buy_price(current_coin + self.config.BRIDGE)
+                )
+                self.logger.info("Ready to start trading")
+
+    def re_initialize_trade_thresholds(self):
+        """
+        Re-initialize all the thresholds ( hard reset - as deleting db )
+        """
+        #updates all ratios
+        #print('************INITIALIZING RATIOS**********')
+        session: Session
+        with self.db.db_session() as session:
+            c1 = aliased(Coin)
+            c2 = aliased(Coin)
+            for pair in session.query(Pair).\
+                join(c1, and_(Pair.from_coin_id == c1.symbol, c1.enabled == True)).\
+                join(c2, and_(Pair.to_coin_id == c2.symbol, c2.enabled == True)).\
+                all():
+                if not pair.from_coin.enabled or not pair.to_coin.enabled:
+                    continue
+                #self.logger.debug(f"Initializing {pair.from_coin} vs {pair.to_coin}", False)
+
+                from_coin_price = self.manager.get_sell_price(pair.from_coin + self.config.BRIDGE)
+                if from_coin_price is None:
+                    # self.logger.debug(
+                    #     "Skipping initializing {}, symbol not found".format(pair.from_coin + self.config.BRIDGE),
+                    #     False
+                    # )
+                    continue
+
+                to_coin_price = self.manager.get_buy_price(pair.to_coin + self.config.BRIDGE)
+                if to_coin_price is None:
+                    # self.logger.debug(
+                    #     "Skipping initializing {}, symbol not found".format(pair.to_coin + self.config.BRIDGE),
+                    #     False
+                    # )
+                    continue
+
+                pair.ratio = (pair.ratio *10 + from_coin_price / to_coin_price)  / 11
+
+    def update_trade_threshold(self, coin: Coin, coin_price: float):
+        pass
