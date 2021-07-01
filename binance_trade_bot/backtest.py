8b0e36221939a7f7920c06a01bd72294754c6243
Nico Lachmuth <nico@nlachmuth.de>
2021-07-01 21:23:20 +0200

    fix backtests

--- binance_trade_bot/backtest.py
+++ binance_trade_bot/backtest.py
@@ -78,7 +78,7 @@ class MockBinanceManager(BinanceAPIManager):
             start_date: datetime = None,
             start_balances: Dict[str, float] = None,
     ):
-        super().__init__(config, db, logger)
+        super().__init__(None, None, config, db, logger, None)
         self.config = config
         self.datetime = start_date or datetime(2021, 1, 1)
         self.balances = start_balances or {config.BRIDGE.symbol: 100}
