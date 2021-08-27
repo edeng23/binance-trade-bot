94a940ce89eb422f1dac1af86c3905d72ac6ca2d
Nico Lachmuth <nico@nlachmuth.de>
2021-08-27 14:23:53 +0200

    update ratio adjust hint

--- binance_trade_bot/strategies/ratio_adjust_strategy.py
+++ binance_trade_bot/strategies/ratio_adjust_strategy.py
@@ -11,7 +11,7 @@ from binance_trade_bot.database import Pair, Coin
 
 class Strategy(AutoTrader):
     def initialize(self):
-        self.logger.info(f"CAUTION: The ratio_adjust strategy is still work in progress and can lead to losses! Use this strategy only if you know what you are doing, did alot of backtests and can live with possible losses.")
+        self.logger.info(f"CAUTION: The ratio_adjust strategy can lead to losses! A lower ratio weight increases the risk! Use this strategy only if you know what you are doing, did alot of backtests and can live with possible losses.")
 
         if self.config.ACCEPT_LOSSES != True:
             self.logger.error("You need accept losses by setting accept_losses=true in the user.cfg or setting the enviroment variable ACCEPT_LOSSES to true in order to use this strategy!")
