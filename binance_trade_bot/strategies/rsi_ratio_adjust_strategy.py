3a2c3f7d5efbf582358664745b01cd0bdc151cfd
SilenTree12th <45297257+SilenTree12th@users.noreply.github.com>
2021-10-20 18:07:20 +0200

    Update rsi_ratio_adjust_strategy.py

--- binance_trade_bot/strategies/rsi_ratio_adjust_strategy.py
+++ binance_trade_bot/strategies/rsi_ratio_adjust_strategy.py
@@ -244,28 +244,29 @@ class Strategy(AutoTrader):
 		
         ratio_dict, prices = self._get_ratios(current_coin, current_coin_price)
         ratio_dict = {k: v for k, v in ratio_dict.items() if v > 0}
+	
+        if ratio_dict:	
+           best_pair = max(ratio_dict, key=ratio_dict.get)
+           to_coin_symbol = best_pair.to_coin_id
+           self.rsi_coin = self.db.get_coin(to_coin_symbol)
 		
-        best_pair = max(ratio_dict, key=ratio_dict.get)
-        to_coin_symbol = best_pair.to_coin_id
-        self.rsi_coin = self.db.get_coin(to_coin_symbol)
-		
-        rsi_price_history = []
-        next_coin_price = self.manager.get_sell_price(self.rsi_coin + self.config.BRIDGE)
+           rsi_price_history = []
+           next_coin_price = self.manager.get_sell_price(self.rsi_coin + self.config.BRIDGE)
 		
         #self.logger.info(f"Starting RSI init: Start Date: {rsi_start_date}, End Date {rsi_end_date}")
 		
-        for result in self.manager.binance_client.get_historical_klines(f"{to_coin_symbol}{self.config.BRIDGE_SYMBOL}", "15m", rsi_start_date_str, rsi_end_date_str, limit=init_rsi_length):                           
-           rsi_price = float(result[1])
-           rsi_price_history.append(rsi_price)
+           for result in self.manager.binance_client.get_historical_klines(f"{to_coin_symbol}{self.config.BRIDGE_SYMBOL}", "15m", rsi_start_date_str, rsi_end_date_str, limit=init_rsi_length):                           
+              rsi_price = float(result[1])
+              rsi_price_history.append(rsi_price)
 		
-        rsi_price_history.append(next_coin_price)
+           rsi_price_history.append(next_coin_price)
 	
-        if len(rsi_price_history) >= init_rsi_length:
-           np_closes = numpy.array(rsi_price_history)
-           rsi = talib.RSI(np_closes, init_rsi_length)
-           self.rsi = rsi[-1]
-           #self.logger.info(f"Finished ratio init...")
+           if len(rsi_price_history) >= init_rsi_length:
+              np_closes = numpy.array(rsi_price_history)
+              rsi = talib.RSI(np_closes, init_rsi_length)
+              self.rsi = rsi[-1]
+              #self.logger.info(f"Finished ratio init...")
 
-        else:
-           self.rsi = 40
-           self.logger.info(f"Not enough data for RSI calculation. Continue scouting...")
+           else:
+              self.rsi = 40
+              self.logger.info(f"Not enough data for RSI calculation. Continue scouting...")
