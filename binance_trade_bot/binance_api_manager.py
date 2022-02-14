4405bedb06c4f4c2e366b72428ce387ed2a51817
MasaiasuOse <45827629+MasaiasuOse@users.noreply.github.com>
2022-02-14 10:08:16 +0100

    Fix/Hardcoded fee in case of TLD=us
    Binance.us doesn't have a a API call for the fee. So it needs to be hardcoded

--- binance_trade_bot/binance_api_manager.py
+++ binance_trade_bot/binance_api_manager.py
@@ -47,6 +47,8 @@ class BinanceAPIManager:
         return self.binance_client.get_bnb_burn_spot_margin()["spotBNBBurn"]
 
     def get_fee(self, origin_coin: Coin, target_coin: Coin, selling: bool):
+        if self.config.BINANCE_TLD != "com":
+            return float(0.001)
         base_fee = self.get_trade_fees()[origin_coin + target_coin]
         if not self.get_using_bnb_for_fees():
             return base_fee
