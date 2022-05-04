61ecf5d2fde614da8f02340989f6ef64b00d84dc
SilenTree12th <45297257+SilenTree12th@users.noreply.github.com>
2022-05-04 11:04:22 +0200

    Update rsi_tema_ratio_auto_adjust_strategy.py

--- binance_trade_bot/strategies/rsi_tema_ratio_auto_adjust_strategy.py
+++ binance_trade_bot/strategies/rsi_tema_ratio_auto_adjust_strategy.py
@@ -67,11 +67,11 @@ class Strategy(AutoTrader):
         # Display on the console, the current coin+Bridge, so users can see *some* activity and not think the bot has
         # stopped. Not logging though to reduce log size.
         print(
-            f"{self.manager.now().replace(microsecond=0)} I am scouting the best trades. ",
+            f"{self.manager.now().replace(microsecond=0)} - I am scouting the best trades. ",
             f"Current coin is: {current_coin + self.config.BRIDGE} with price direction: {(self.from_coin_prices[-1] - self.mean_price):.3E} ",
             f"(jump when negative) " if self.from_coin_prices[-1] - self.mean_price > 0 else "",
             f"Next best coin is: {self.rsi_coin} with RSI: {round(self.rsi, 3)} and price direction: {(self.to_coin_price - self.tema):.3E} " if self.rsi else "",
-            f"(jump when positive) "
+            f"(jump when positive) " if self.to_coin_price - self.tema < 0 else "",
             f"Current ratio weight is: {self.auto_weight} ",
             end='\r',
         )
