3f4e1e2294646364090810f29f2b3aa9923dfc6d
SilenTree12th <45297257+SilenTree12th@users.noreply.github.com>
2022-05-06 23:57:21 +0200

    Update rsi_stop_loss_strategy.py

--- binance_trade_bot/strategies/rsi_stop_loss_strategy.py
+++ binance_trade_bot/strategies/rsi_stop_loss_strategy.py
@@ -75,7 +75,7 @@ class Strategy(AutoTrader):
         # Display on the console, the current coin+Bridge, so users can see *some* activity and not think the bot has
         # stopped. Not logging though to reduce log size.
         print(
-            f"{self.manager.now()} - Scouting. ",
+            f"{self.manager.now()} - ",
             f"Current ratio weight is: {self.auto_weight} ",
             f"Current coin is: {current_coin + self.config.BRIDGE} with price direction: {(self.from_coin_prices[-1] - self.mean_price):.3E} ",
             f"(jump when negative) " if self.from_coin_prices[-1] - self.mean_price > 0 else "",
