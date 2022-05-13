6b3179edc2dd76c6aabc645555206b1132c481d3
SilenTree12th <45297257+SilenTree12th@users.noreply.github.com>
2022-05-13 14:16:21 +0200

    Update test_strategy.py

--- binance_trade_bot/strategies/test_strategy.py
+++ binance_trade_bot/strategies/test_strategy.py
@@ -154,7 +154,7 @@ class Strategy(AutoTrader):
                     self.logger.info("Couldn't sell, going back to scouting mode...")
                     self.panicked = False
                 else:
-                    self.panic_time = self.manager.now().replace(second=0, microsecond=0) + timedelta(minutes=(int(self.config.RSI_CANDLE_TYPE))
+                    self.panic_time = self.manager.now().replace(second=0, microsecond=0) + timedelta(minutes=int(self.config.RSI_CANDLE_TYPE))
                     self.panic_prices = []
                     self.panic_prices = deque(maxlen=int(self.config.MAX_IDLE_HOURS) * 1800)
 		
@@ -167,7 +167,7 @@ class Strategy(AutoTrader):
                     self.logger.info("Couldn't buy, going back to panic mode...")
                     self.panicked = True
                 else:
-                    self.panic_time = self.manager.now().replace(second=0, microsecond=0) + timedelta(minutes=(int(self.config.RSI_CANDLE_TYPE))
+                    self.panic_time = self.manager.now().replace(second=0, microsecond=0) + timedelta(minutes=int(self.config.RSI_CANDLE_TYPE))
                     self.panic_prices = []
                     self.panic_prices = deque(maxlen=int(self.config.MAX_IDLE_HOURS) * 1800)
 		
