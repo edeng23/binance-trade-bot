51556c5aa54fbc652c95e8acaf2cc86db99e8a01
SilenTree12th <45297257+SilenTree12th@users.noreply.github.com>
2022-05-20 00:06:43 +0200

    Update test_strategy.py

--- binance_trade_bot/strategies/test_strategy.py
+++ binance_trade_bot/strategies/test_strategy.py
@@ -68,7 +68,8 @@ class Strategy(AutoTrader):
 		
         if base_time >= allowed_rsi_time:
             #self.panic_prices.append(self.manager.get_buy_price(current_coin + self.config.BRIDGE))
-            self.from_coin_prices.append(self.manager.get_buy_price(current_coin + self.config.BRIDGE))
+            if self.from_coin_prices[-1] != self.manager.get_buy_price(current_coin + self.config.BRIDGE)
+                self.from_coin_prices.append(self.manager.get_buy_price(current_coin + self.config.BRIDGE))
             self.mean_price = numpy.mean(self.from_coin_prices)
             self.from_coin_direction = self.from_coin_prices[-1] / self.mean_price * 100 - 100
             self.rsi_calc()
@@ -152,16 +153,16 @@ class Strategy(AutoTrader):
                     self.logger.info("Not enough balance")
                     self.panicked = False
                     self.panic_time = self.manager.now().replace(second=0, microsecond=0) + timedelta(minutes=int(self.config.RSI_CANDLE_TYPE))
-                    #self.from_coin_prices = []
-                    #self.from_coin_prices = deque(maxlen=int(self.config.MAX_IDLE_HOURS) * 1800)
+                    self.from_coin_prices = []
+                    self.from_coin_prices = deque(maxlen=int(self.config.MAX_IDLE_HOURS) * 1800)
 
                 if can_sell and self.manager.sell_alt(panic_pair.from_coin, self.config.BRIDGE, current_coin_price) is None:
                     self.logger.info("Couldn't sell, going back to scouting mode...")
                     self.panicked = False
                 else:
                     self.panic_time = self.manager.now().replace(second=0, microsecond=0) + timedelta(minutes=int(self.config.RSI_CANDLE_TYPE))
-                    #self.from_coin_prices = []
-                    #self.from_coin_prices = deque(maxlen=int(self.config.MAX_IDLE_HOURS) * 1800)
+                    self.from_coin_prices = []
+                    self.from_coin_prices = deque(maxlen=int(self.config.MAX_IDLE_HOURS) * 1800)
 		
         elif base_time >= panic_time and self.panicked:
             self.panic_time = self.manager.now().replace(second=0, microsecond=0) + timedelta(minutes=1)
