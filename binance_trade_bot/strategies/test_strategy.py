5cbfc3f96ca273c2522b0641620de29307a6a33b
SilenTree12th <45297257+SilenTree12th@users.noreply.github.com>
2022-08-17 15:16:39 +0200

    Update test_strategy.py

--- binance_trade_bot/strategies/test_strategy.py
+++ binance_trade_bot/strategies/test_strategy.py
@@ -155,7 +155,7 @@ class Strategy(AutoTrader):
             
         if self.rsi:
            if self.panicked:
-                if self.to_coin_direction >= 0 and (self.rsi > self.pre_rsi <= 30 or self.pre_rsi < self.rsi > 50 or self.rsi < 20):
+                if self.to_coin_direction >= 0 and (self.rsi > self.pre_rsi <= 30 or self.pre_rsi < self.rsi > 50) or self.rsi < 20:
                         print("")
                         self.from_coin_prices = []
                         self.meter_prices = []
@@ -169,7 +169,7 @@ class Strategy(AutoTrader):
                         self.reinit_idle = self.manager.now().replace(second=0, microsecond=0) + timedelta(hours=int(self.config.MAX_IDLE_HOURS))
                         self.panic_time = self.manager.now().replace(second=0, microsecond=0) + timedelta(minutes=int(self.config.RSI_CANDLE_TYPE))
            else:
-                if self.from_coin_direction - self.meter <= self.to_coin_direction >= 0 and (self.pre_rsi < self.rsi <= 30 or self.pre_rsi < self.rsi > 50 or self.rsi < 20):
+                if self.from_coin_direction - self.meter <= self.to_coin_direction >= 0 and (self.pre_rsi < self.rsi <= 30 or self.pre_rsi < self.rsi > 50) or self.rsi < 20:
                         print("")
                         self.from_coin_prices = []
                         self.meter_prices = []
