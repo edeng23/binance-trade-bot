f2a2b7ff03473198bd7419fe5cf7b1598092cf13
SilenTree12th <45297257+SilenTree12th@users.noreply.github.com>
2022-09-26 12:06:49 +0200

    Update test_strategy.py

--- binance_trade_bot/strategies/test_strategy.py
+++ binance_trade_bot/strategies/test_strategy.py
@@ -96,7 +96,7 @@ class Strategy(AutoTrader):
         print(
             f"{self.manager.now().replace(microsecond=0)} - " ,
             f"Threshold: {round(self.from_coin_direction - self.dir_threshold, 3)}% " if self.dir_threshold != 0 else "",
-            f"Bottom: {self.active_threshold} {self.config.BRIDGE} " if self.active_threshold != 0 else "",
+            f"Bottom: {self.active_threshold} {self.config.BRIDGE} ",
             f"Long position " if not self.panicked else "Short position ",
             f"Current ratio weight: {self.auto_weight} ",
             f"Current coin: {current_coin + self.config.BRIDGE} with RSI: {round(self.rv_rsi, 3)} price direction: {round(self.from_coin_direction, 3)}% ",
