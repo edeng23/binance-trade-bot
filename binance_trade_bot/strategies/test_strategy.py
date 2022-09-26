3f8f0aa30a05758c860af88bf6e351dc92e266e1
SilenTree12th <45297257+SilenTree12th@users.noreply.github.com>
2022-09-26 10:21:47 +0200

    Update test_strategy.py

--- binance_trade_bot/strategies/test_strategy.py
+++ binance_trade_bot/strategies/test_strategy.py
@@ -471,18 +471,18 @@ class Strategy(AutoTrader):
             for result in self.manager.binance_client.get_historical_klines(f"{current_coin_symbol}{self.config.BRIDGE_SYMBOL}", rsi_string, rsi_start_date_str, rsi_end_date_str, limit=init_rsi_length*5):                           
                 rsi_price = float(result[1])
                 self.reverse_price_history.append(rsi_price)
-           
-                if self.reverse_price_history[-1] > self.reverse_price_history[-2]:
-                    AUC = K * (self.reverse_price_history[-1] - self.reverse_price_history[-2]) + (1 - K) * AUC
-                    ADC = (1 -K) * ADC
-                else:
-                    AUC = (1 - K) * AUC
-                    ADC = K * (self.reverse_price_history[-2] - self.reverse_price_history[-1]) + (1 - K) * ADC
-        
-            del self.reverse_price_history[0]
-        
+                
         else:
             self.reverse_price_history[-1] = self.from_coin_price
+           
+        if self.reverse_price_history[-1] > self.reverse_price_history[-2]:
+            AUC = K * (self.reverse_price_history[-1] - self.reverse_price_history[-2]) + (1 - K) * AUC
+            ADC = (1 -K) * ADC
+        else:
+            AUC = (1 - K) * AUC
+            ADC = K * (self.reverse_price_history[-2] - self.reverse_price_history[-1]) + (1 - K) * ADC
+        
+        del self.reverse_price_history[0]
         
         Val_70 = (init_rsi_length - 1) * (ADC * 70 / 30 - AUC)
         Val_50 = (init_rsi_length - 1) * (ADC - AUC)
