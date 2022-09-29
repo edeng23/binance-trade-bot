bdd757c2b9898d8b956aa19f36286d85cdb4433b
SilenTree12th <45297257+SilenTree12th@users.noreply.github.com>
2022-09-29 02:41:59 +0200

    Update test_strategy.py

--- binance_trade_bot/strategies/test_strategy.py
+++ binance_trade_bot/strategies/test_strategy.py
@@ -116,16 +116,16 @@ class Strategy(AutoTrader):
         
             
         if self.rsi:
-        #    if self.panicked:
+            if self.panicked:
         #         if self.to_coin_direction >= 0 and (self.rsi > self.pre_rsi <= 30 or self.pre_rsi < self.rsi > 50) or self.rsi < 20:
-        #             print("")
-        #             self.auto_weight = int(self.config.RATIO_ADJUST_WEIGHT)
-        #             self.panicked = False
-        #             self._jump_to_best_coin(current_coin, self.from_coin_price)
-        #             self.active_threshold = 0
-        #             self.dir_threshold = 0
-        #             self.reinit_idle = self.manager.now().replace(second=0, microsecond=0) + timedelta(hours=int(self.config.MAX_IDLE_HOURS))
-        #             self.panic_time = self.manager.now().replace(second=0, microsecond=0) + timedelta(minutes=int(self.config.RSI_CANDLE_TYPE))
+              print("")
+              self.auto_weight = int(self.config.RATIO_ADJUST_WEIGHT)
+              self.panicked = False
+              self._jump_to_best_coin(current_coin, self.from_coin_price)
+              self.active_threshold = 0
+              self.dir_threshold = 0
+              self.reinit_idle = self.manager.now().replace(second=0, microsecond=0) + timedelta(hours=int(self.config.MAX_IDLE_HOURS))
+              self.panic_time = self.manager.now().replace(second=0, microsecond=0) + timedelta(minutes=int(self.config.RSI_CANDLE_TYPE))
 
         #    else:
             if self.from_coin_direction <= self.to_coin_direction >= 0 and (self.pre_rsi < self.rsi <= 30 or self.pre_rsi < self.rsi > 50) or self.rsi < 20:
