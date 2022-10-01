bd0e05ef03ee67d928ee7703c1b2872396e35131
SilenTree12th <45297257+SilenTree12th@users.noreply.github.com>
2022-10-01 14:50:40 +0200

    Update test_strategy.py

--- binance_trade_bot/strategies/test_strategy.py
+++ binance_trade_bot/strategies/test_strategy.py
@@ -119,26 +119,26 @@ class Strategy(AutoTrader):
             
         if self.rsi:
             if self.panicked:
-        #         if self.to_coin_direction >= 0 and (self.rsi > self.pre_rsi <= 30 or self.pre_rsi < self.rsi > 50) or self.rsi < 20:
-              print("")
-              self.auto_weight = int(self.config.RATIO_ADJUST_WEIGHT)
-              self.panicked = False
-              self.transaction_through_bridge(self.best_pair, round(max(self.from_coin_price, self.rv_tema), self.d), round(min(self.to_coin_price, self.tema), self.d))
-              self.active_threshold = 0
-              self.dir_threshold = 0
-              self.reinit_idle = self.manager.now().replace(second=0, microsecond=0) + timedelta(hours=int(self.config.MAX_IDLE_HOURS))
-              self.panic_time = self.manager.now().replace(second=0, microsecond=0) + timedelta(minutes=1)#int(self.config.RSI_CANDLE_TYPE))
+                if self.to_coin_direction >= 0 and (self.rsi > self.pre_rsi <= 30 or self.pre_rsi < self.rsi > 50) or self.rsi < 20:
+                    print("")
+                    self.auto_weight = int(self.config.RATIO_ADJUST_WEIGHT)
+                    self.panicked = False
+                    self.transaction_through_bridge(self.best_pair, round(max(self.from_coin_price, self.rv_tema), self.d), round(min(self.to_coin_price, self.tema), self.d))
+                    self.active_threshold = 0
+                    self.dir_threshold = 0
+                    self.reinit_idle = self.manager.now().replace(second=0, microsecond=0) + timedelta(hours=int(self.config.MAX_IDLE_HOURS))
+                    self.panic_time = self.manager.now().replace(second=0, microsecond=0) + timedelta(minutes=int(self.config.RSI_CANDLE_TYPE))
 
-        #    else:
-            elif self.from_coin_direction <= self.to_coin_direction >= 0 and (self.pre_rsi < self.rsi <= 30 or self.pre_rsi < self.rsi > 50) or self.rsi < 20:
-                print("")
-                self.auto_weight = int(self.config.RATIO_ADJUST_WEIGHT)
-                self.panicked = False
-                self.transaction_through_bridge(self.best_pair, round(max(self.from_coin_price, self.rv_tema), self.d), round(min(self.to_coin_price, self.tema), self.v))
-                self.active_threshold = 0
-                self.dir_threshold = 0
-                self.reinit_idle = self.manager.now().replace(second=0, microsecond=0) + timedelta(hours=int(self.config.MAX_IDLE_HOURS))
-                self.panic_time = self.manager.now().replace(second=0, microsecond=0) + timedelta(minutes=1)#int(self.config.RSI_CANDLE_TYPE))
+            else:
+                elif self.from_coin_direction <= self.to_coin_direction >= 0 and (self.pre_rsi < self.rsi <= 30 or self.pre_rsi < self.rsi > 50) or self.rsi < 20:
+                    print("")
+                    self.auto_weight = int(self.config.RATIO_ADJUST_WEIGHT)
+                    self.panicked = False
+                    self.transaction_through_bridge(self.best_pair, round(max(self.from_coin_price, self.rv_tema), self.d), round(min(self.to_coin_price, self.tema), self.v))
+                    self.active_threshold = 0
+                    self.dir_threshold = 0
+                    self.reinit_idle = self.manager.now().replace(second=0, microsecond=0) + timedelta(hours=int(self.config.MAX_IDLE_HOURS))
+                    self.panic_time = self.manager.now().replace(second=0, microsecond=0) + timedelta(minutes=int(self.config.RSI_CANDLE_TYPE))
                         
         #if base_time >= self.panic_time and self.manager.get_currency_balance(panic_pair.from_coin.symbol) == 0 and not self.panicked:
             #self.panicked = True
