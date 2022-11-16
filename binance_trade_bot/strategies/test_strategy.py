bcd389d2790577208c415bbc0e38c3d78caf9e35
SilenTree12th <45297257+SilenTree12th@users.noreply.github.com>
2022-11-16 17:08:19 +0100

    Update test_strategy.py

--- binance_trade_bot/strategies/test_strategy.py
+++ binance_trade_bot/strategies/test_strategy.py
@@ -519,7 +519,8 @@ class Strategy(AutoTrader):
                 
         else:
             self.reverse_price_history[-1] = float(self.from_coin_price)
-            self.volume[-1] = self.volume[-1]+self.manager.binance_client.get_historical_klines(f"{current_coin_symbol}{self.config.BRIDGE_SYMBOL}", "1m")[5]
+            result = self.manager.binance_client.get_historical_klines(f"{current_coin_symbol}{self.config.BRIDGE_SYMBOL}", "1m")
+            self.volume[-1] = self.volume[-1] + result[5]
         
         if len(self.reverse_price_history) >= init_rsi_length:
             rv_closes = numpy.array(self.reverse_price_history)
