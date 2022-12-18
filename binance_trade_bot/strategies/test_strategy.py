11eeadf0e25fccc69caa9e3e1d8554092dd07d79
SilenTree12th <45297257+SilenTree12th@users.noreply.github.com>
2022-12-18 03:12:12 +0100

    Update test_strategy.py

--- binance_trade_bot/strategies/test_strategy.py
+++ binance_trade_bot/strategies/test_strategy.py
@@ -557,7 +557,7 @@ class Strategy(AutoTrader):
             allocs = numpy.digitize(hlc, bins) - 1
             position_now = numpy.digitize(self.from_coin_price, bins) - 1
 
-            hist = dict()
+            hist = {}
             hist = {i: 0 for i in range(len(bins)+1)}
             for a,vol in zip(allocs, volume):
                 if not a in hist:
