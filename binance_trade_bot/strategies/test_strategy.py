bb264d3568fca9a3f48990662d00fe2a997ccd8f
SilenTree12th <45297257+SilenTree12th@users.noreply.github.com>
2023-02-01 13:25:03 +0100

    Update test_strategy.py

--- binance_trade_bot/strategies/test_strategy.py
+++ binance_trade_bot/strategies/test_strategy.py
@@ -112,7 +112,7 @@ class Strategy(AutoTrader):
         print(
             f"{self.manager.now().strftime('%Y-%m-%d %H:%M:%S')} - " ,
             f"{Fore.CYAN}Holding{Style.RESET_ALL} " if not self.panicked else f"{Fore.CYAN}Not holding{Style.RESET_ALL} ",
-            f"{Fore.CYAN}Pivot point{Style.RESET_ALL} " if self.equi else f"{Fore.CYAN}Moving{Style.RESET_ALL} ",
+            f"{Fore.CYAN}Pivot{Style.RESET_ALL} " if self.equi else f"{Fore.CYAN}Moving{Style.RESET_ALL} ",
             f"Threshold: {Fore.CYAN}{round(self.from_coin_direction - self.dir_threshold, 3)}%{Style.RESET_ALL} " if self.dir_threshold != 0 else f"",
             f"Bottom: {Fore.CYAN}{round(self.active_threshold, self.d)}{Style.RESET_ALL} " if not self.panicked else f"Top: {Fore.CYAN}{round(self.active_threshold, self.d)}{Style.RESET_ALL} ",
             f"Ratio weight: {Fore.CYAN}{self.auto_weight}{Style.RESET_ALL} ",
