52204a022fa0dcd279d55367fa746cd1d33bd159
SilenTree12th <45297257+SilenTree12th@users.noreply.github.com>
2023-03-28 09:07:19 +0200

    Update test_strategy.py

--- binance_trade_bot/strategies/test_strategy.py
+++ binance_trade_bot/strategies/test_strategy.py
@@ -575,7 +575,7 @@ class Strategy(AutoTrader):
             hlc = []
             for values in comb:
                 hlc.append(sum(values) / 3)
-            stdev = int((max(self.highs) - min(self.lows)) / (3*st.stdev(numpy.array(self.reverse_price_history[-1 * self.calcval:])))) + 1
+            stdev = int((max(self.highs) - min(self.lows)) / (3*st.stdev(numpy.array(self.reverse_price_history[-1 * self.calcval:]))))
             bins_a = stdev
             count, bins = numpy.histogram(hlc, bins=bins_a)
             allocs = numpy.digitize(hlc, bins) - 1
