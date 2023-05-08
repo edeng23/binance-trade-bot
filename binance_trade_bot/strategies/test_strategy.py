a940473dfe62fe72ed75877357df07a97be46b54
SilenTree12th <45297257+SilenTree12th@users.noreply.github.com>
2023-05-08 08:48:29 +0200

    Update test_strategy.py

--- binance_trade_bot/strategies/test_strategy.py
+++ binance_trade_bot/strategies/test_strategy.py
@@ -592,7 +592,7 @@ class Strategy(AutoTrader):
                 hlc.append(sum(values) / 3)
                 
             try:
-                bins_a = int((max(self.highs) - min(self.lows)) / (st.stdev(numpy.array(self.reverse_price_history[-1 * self.calcval:])))) + 1
+                bins_a = int((max(self.highs) - min(self.lows)) / (st.stdev(numpy.array(self.reverse_price_history[-1-self.calcval:-2])))) + 1
             except:
                 bins_a = self.config.RSI_LENGTH + 1
             
