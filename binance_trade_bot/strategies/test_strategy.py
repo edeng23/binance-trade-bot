5e9ed068785702ea3b86824007c03831f0bcbf52
SilenTree12th <45297257+SilenTree12th@users.noreply.github.com>
2023-05-10 10:34:04 +0200

    Update test_strategy.py

--- binance_trade_bot/strategies/test_strategy.py
+++ binance_trade_bot/strategies/test_strategy.py
@@ -241,6 +241,7 @@ class Strategy(AutoTrader):
                     elif self.manager.sell_alt(panic_pair.from_coin, self.config.BRIDGE, self.from_coin_price) is None:
                         self.logger.info("Couldn't sell, going back to scouting mode...")
                         self.panicked = False
+                        self.panic_time = self.manager.now().replace(second=0, microsecond=0) + timedelta(minutes=1)
                         #self.active_threshold = 0
 
                     else:
@@ -282,6 +283,7 @@ class Strategy(AutoTrader):
                 if self.manager.buy_alt(panic_pair.from_coin, self.config.BRIDGE, self.from_coin_price) is None:
                     self.logger.info("Couldn't buy, going back to panic mode...")
                     self.panicked = True
+                    self.panic_time = self.manager.now().replace(second=0, microsecond=0) + timedelta(minutes=1)
                     #self.active_threshold = max(self.reverse_price_history) * 3
 
                 else:
